4e42391f1be29dd72f571a3e78edfdb46b30200e
Sylvain Hellegouarch <sh@defuze.org>
2018-09-13 23:30:46 +0200

    Add version into name
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- ci.bash
+++ ci.bash
@@ -7,12 +7,18 @@ function build () {
     export CHAOSTOOLKIT=`which chaos`
     sed -i "s|<CHAOS_PATH>|${CHAOSTOOLKIT}|" chaos.spec
 
+    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        VERSION=$TRAVIS_TAG
+    else
+        VERSION=$TRAVIS_COMMIT
+    fi
+
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         pyinstaller chaos.spec
-        mv dist/chaos dist/chaos-darwin-amd64
+        mv dist/chaos dist/chaos-$VERSION-darwin-amd64
     else
         pyinstaller chaos.spec
-        mv dist/chaos dist/chaos-linux-amd64
+        mv dist/chaos dist/chaos-$VERSION-linux-amd64
     fi
 }
 
