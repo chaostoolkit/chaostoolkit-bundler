9eeae192412b01aafc10a6ee0f542da858d0817f
Sylvain Hellegouarch <sh@defuze.org>
2021-09-03 17:06:50 +0200

    export packages metadat
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- hooks/hook-chaosgcp.py
+++ hooks/hook-chaosgcp.py
@@ -1,5 +1,7 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules, \
+    copy_metadata
 
+datas = copy_metadata('chaostoolkit-google-cloud-platform', recursive=True)
 hiddenimports = (
     collect_submodules('chaosgcp')
 )
--- hooks/hook-chaosk8s.py
+++ hooks/hook-chaosk8s.py
@@ -1,5 +1,7 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules, \
+    copy_metadata
 
+datas = copy_metadata('chaostoolkit-kubernetes', recursive=True)
 hiddenimports = (
     collect_submodules('chaosk8s')
 )
