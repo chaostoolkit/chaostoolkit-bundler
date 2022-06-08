bb51e55f5ac45b66d81485c82215dd0aadab3ca8
Sylvain Hellegouarch <sh@defuze.org>
2022-06-08 22:48:41 +0200

    fix missing metadata
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- hooks/hook-chaostracing.py
+++ hooks/hook-chaostracing.py
@@ -2,7 +2,14 @@ from PyInstaller.utils.hooks import collect_data_files, collect_submodules, \
     copy_metadata
 
 datas = copy_metadata('chaostoolkit-opentracing', recursive=True) + \
-    copy_metadata('opentelemetry-api', recursive=True)
+    copy_metadata('opentelemetry-api', recursive=True) + \
+    copy_metadata('opentelemetry-exporter-jaeger-proto-grpc', recursive=True) + \
+    copy_metadata('opentelemetry-exporter-otlp-proto-grpc', recursive=True) + \
+    copy_metadata('opentelemetry-exporter-otlp-proto-http', recursive=True) + \
+    copy_metadata('opentelemetry-opentracing-shim', recursive=True) + \
+    copy_metadata('opentelemetry-exporter-jaeger-thrift', recursive=True) + \
+    copy_metadata('opentelemetry-propagator-b3', recursive=True) + \
+    copy_metadata('opentelemetry-sdk', recursive=True)
 hiddenimports = (
     collect_submodules('chaostracing')
 ) + (
