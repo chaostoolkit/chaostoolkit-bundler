5d7750009e5688e9455376e7d9a3d5e2fa80d4fd
Sylvain Hellegouarch <sh@defuze.org>
2022-06-08 21:53:50 +0200

    fix typeerror
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- hooks/hook-chaostracing.py
+++ hooks/hook-chaostracing.py
@@ -3,6 +3,7 @@ from PyInstaller.utils.hooks import collect_data_files, collect_submodules, \
 
 datas = copy_metadata('chaostoolkit-opentracing', recursive=True)
 hiddenimports = (
-    collect_submodules('chaostracing'),
+    collect_submodules('chaostracing')
+) + (
     collect_submodules('opentelemetry')
 )
