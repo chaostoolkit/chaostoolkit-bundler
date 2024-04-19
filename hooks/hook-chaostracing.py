f77a814af1a1f7f11ef0512ede80c367359dda0a
Sylvain Hellegouarch <sh@defuze.org>
2024-04-19 15:55:22 +0200

    switch to pdm
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- hooks/hook-chaostracing.py
+++ hooks/hook-chaostracing.py
@@ -3,13 +3,18 @@ from PyInstaller.utils.hooks import collect_data_files, collect_submodules, \
 
 datas = copy_metadata('chaostoolkit-opentracing', recursive=True) + \
     copy_metadata('opentelemetry-api', recursive=True) + \
-    copy_metadata('opentelemetry-exporter-jaeger-proto-grpc', recursive=True) + \
-    copy_metadata('opentelemetry-exporter-otlp-proto-grpc', recursive=True) + \
+    copy_metadata('opentelemetry-sdk', recursive=True) + \
     copy_metadata('opentelemetry-exporter-otlp-proto-http', recursive=True) + \
-    copy_metadata('opentelemetry-opentracing-shim', recursive=True) + \
-    copy_metadata('opentelemetry-exporter-jaeger-thrift', recursive=True) + \
     copy_metadata('opentelemetry-propagator-b3', recursive=True) + \
-    copy_metadata('opentelemetry-sdk', recursive=True)
+    copy_metadata('opentelemetry-semantic-conventions', recursive=True) + \
+    copy_metadata('opentelemetry-instrumentation-httpx', recursive=True) + \
+    copy_metadata('opentelemetry-instrumentation-requests', recursive=True) + \
+    copy_metadata('opentelemetry-instrumentation-botocore', recursive=True) + \
+    copy_metadata('opentelemetry-exporter-gcp-trace', recursive=True) + \
+    copy_metadata('opentelemetry-resourcedetector-gcp', recursive=True) + \
+    copy_metadata('opentelemetry-sdk-extension-aws', recursive=True) + \
+    copy_metadata('opentelemetry-propagator-aws-xray', recursive=True) + \
+    copy_metadata('opentelemetry-instrumentation-urllib3', recursive=True)
 hiddenimports = (
     collect_submodules('chaostracing')
 ) + (
