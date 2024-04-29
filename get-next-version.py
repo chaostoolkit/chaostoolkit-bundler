6da1ed32a476eca8e91eddeec74f927b9bb660a8
Sylvain Hellegouarch <sh@defuze.org>
2024-04-29 15:20:43 +0200

    reuse file afterall
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- get-next-version.py
+++ get-next-version.py
@@ -1,22 +1,21 @@
 from datetime import date
-import subprocess
 
 
 def run():
-    p = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True)
-    current = p.stdout.decode("utf-8").strip()
-    new_version = date.today().strftime("%Y.%m.%d")
+    with open('VERSION') as f:
+        current = f.read().strip()
+        new_version = date.today().strftime("%Y.%m.%d")
 
-    if current and current.startswith(new_version):
-        if new_version == current:
-            index = 0
-        else:
-            _, index = current.rsplit('.', 1)
-        index = int(index) + 1
-        print('{}.{}'.format(new_version, index))
-        return
+        if current and current.startswith(new_version):
+            if new_version == current:
+                index = 0
+            else:
+                _, index = current.rsplit('.', 1)
+            index = int(index) + 1
+            print('{}.{}'.format(new_version, index))
+            return
 
-    print(new_version)
+        print(new_version)
 
 
 if __name__ == '__main__':
